bde812bd8260d4785eb8dedc35d35c5007dacf57
Krzysztof Zych <krzysztof.zych.programista@gmail.com>
2025-09-08 21:38:32 +0200

    Add partials/_toggleButtons for test Try to move as much as possible to /partials

--- src/App/views/balance.php
+++ src/App/views/balance.php
@@ -21,10 +21,11 @@
         </div>
       </div>
 	  
-	    <div class="btn-group m-4" role="group" aria-label="Przełącznik raportu">
+      <?php include $this->resolve("partials/_toggleButtons.php"); ?>
+	    <!-- <div class="btn-group m-4" role="group" aria-label="Przełącznik raportu">
         <a href="./balance2" class="btn btn-lg btn-outline-primary" id="button1" onclick="toggleButtons('button1', 'button2')">Podział na kategorie</a>
         <a href="./balance" class="btn btn-lg btn-primary" id="button2" onclick="toggleButtons('button2', 'button1')">Wyświetl wszystko</a>
-		</div>  
+		</div>   -->
 	  
     </section>
     <div class="album py-3 bg-body-tertiary">
---
+++ src/App/views/partials/_toggleButtons.php
@@ -0,0 +1,4 @@
+	    <div class="btn-group m-4" role="group" aria-label="Przełącznik raportu">
+        <a href="./balance2" class="btn btn-lg btn-outline-primary" id="button1" onclick="toggleButtons('button1', 'button2')">Podział na kategorie</a>
+        <a href="./balance" class="btn btn-lg btn-primary" id="button2" onclick="toggleButtons('button2', 'button1')">Wyświetl wszystko</a>
+		</div>  
