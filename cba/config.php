1988a719a6ece4893deee298f50c2ac8825ff75d
chriszych <krzysztof.zych.programista@gmail.com>
2025-09-22 23:25:59 +0200

    Delete unnecessary files
    Delete unnecessary files

--- cba/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	return ['host' => 'localhost', 
-			'user' => 'kriss',
-			'password' => 'Kriss4mysql.',
-			'database' => 'krisstoph'
-			];
--- dhosting/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	return ['host' => 'finanse.krzysztof-zych.profesjonalnyprogramista.pl.mysql.dhosting.pl', 
-			'user' => 'iemah9_finansek',
-			'password' => 'ajieHee8coof',
-			'database' => 'vaef3i_finansek'
-			];
