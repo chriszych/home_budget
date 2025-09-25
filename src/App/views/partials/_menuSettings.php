bd543e78a4321241aee1c599b43b3e635e1068f4
chriszych <krzysztof.zych.programista@gmail.com>
2025-09-25 22:54:42 +0200

    Small bugs fixed on the balance menu
    Small bugs fixed on the balance menu

--- src/App/views/partials/_menuSettings.php
+++ src/App/views/partials/_menuSettings.php
@@ -1,12 +1,12 @@
 
-      <!-- <div class="navbar navbar-expand-lg rounded"> -->
+      <!-- <div class="navbar navbar-expand-lg rounded">
       <div class="navbar navbar-expand rounded">
-        <!-- <div class="container-fluid"> -->
+        <div class="container-fluid">
           <div class="d-flex align-center" id="buttonNavbar">
-          <!-- <div class="collapse navbar-collapse d-lg-flex" id="buttonNavbar"> -->
+          <div class="collapse navbar-collapse d-lg-flex" id="buttonNavbar">
 
             <ul class="navbar-nav col-12 justify-content-around align-center">
-            <!-- <ul class="navbar-nav col-lg-9 justify-content-lg-end"> -->
+            <ul class="navbar-nav col-lg-9 justify-content-lg-end">
 
               <li class="nav-item">
                 <a class="btn btn-lg btn-primary m-1" href="#">Aktualny miesiąc</a>
@@ -26,5 +26,44 @@
 
             </ul>
           </div>
-        <!-- </div> -->
-      </div>
+         </div> 
+      </div> -->
+
+      <!-- <div class="navbar navbar-expand rounded justify-content-center">
+  <div class="d-flex justify-content-center w-100" id="buttonNavbar">
+    <ul class="navbar-nav d-flex flex-row justify-content-center">
+      <li class="nav-item">
+        <a class="btn btn-lg btn-primary m-1" href="#">Aktualny miesiąc</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-lg btn-outline-primary m-1" href="#">Poprzedni miesiąc</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-lg btn-outline-primary m-1" href="#">Aktualny rok</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-lg btn-outline-primary m-1" href="#">Dowolna data</a>
+      </li>
+    </ul>
+  </div>
+</div> -->
+
+<div class="navbar navbar-expand rounded justify-content-center">
+  <div class="d-flex justify-content-center w-100" id="buttonNavbar">
+    <ul class="navbar-nav d-flex flex-column flex-lg-row justify-content-center">
+      <li class="nav-item">
+        <a class="btn btn-lg btn-primary m-2 w-100" href="#">Aktualny miesiąc</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-lg btn-outline-primary m-2 w-100" href="#">Poprzedni miesiąc</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-lg btn-outline-primary m-2 w-100" href="#">Aktualny rok</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-lg btn-outline-primary m-2 w-100" href="#">Dowolna data</a>
+      </li>
+    </ul>
+  </div>
+</div>
+
